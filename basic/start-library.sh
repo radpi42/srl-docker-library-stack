324fb5ce771127be932ebb41d859c62e378cdc09
radpivm <radpivm@gmail.com>
2022-11-16 23:04:19 +0000

    cleaned

--- basic/start-library.sh
+++ basic/start-library.sh
@@ -2,6 +2,8 @@
 
 docker network create library-net
 
+mkdir $DIR/services/mangment/homepage/config
+
 docker compose pull
 
 docker compose up -d
---
+++ start-library-managment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker network create library-net
+
+#mkdir $DIR/services/mangment/homepage/config
+
+docker compose pull
+
+docker compose up -d 
